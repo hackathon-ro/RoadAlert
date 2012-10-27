a571b68983391cda11cfef758bf5e374114c5e7c
Catalin Morosan <catalin.morosan@gmail.com>
2012-10-27 21:41:04 +0300

    Added polaris map library.

--- android/core/src/com/makanstudios/roadalert/ui/activity/MainActivity.java
+++ android/core/src/com/makanstudios/roadalert/ui/activity/MainActivity.java
@@ -1,104 +1,183 @@
 
 package com.makanstudios.roadalert.ui.activity;
 
-import android.content.Context;
-import android.os.AsyncTask;
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.google.android.gcm.GCMRegistrar;
-import com.kaciula.utils.misc.LogUtils;
+import com.cyrilmottier.polaris.Annotation;
+import com.cyrilmottier.polaris.MapCalloutView;
+import com.cyrilmottier.polaris.MapViewUtils;
+import com.cyrilmottier.polaris.PolarisMapView;
+import com.cyrilmottier.polaris.PolarisMapView.OnAnnotationSelectionChangedListener;
+import com.cyrilmottier.polaris.PolarisMapView.OnRegionChangedListener;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
 import com.makanstudios.roadalert.R;
-import com.makanstudios.roadalert.net.NetService;
-import com.makanstudios.roadalert.ui.misc.RoadAlertActivity;
-import com.makanstudios.roadalert.utils.AppParams;
-import com.makanstudios.roadalert.utils.CustomConstants;
+import com.makanstudios.roadalert.utils.Config;
 
-public class MainActivity extends RoadAlertActivity {
+public class MainActivity extends MapActivity implements OnRegionChangedListener,
+        OnAnnotationSelectionChangedListener {
 
-    private AsyncTask<Void, Void, Void> mRegisterTask;
+    private static final String LOG_TAG = "MainActivity";
+
+    // @formatter:off
+    private static final Annotation[] sFrance = {
+            new Annotation(
+                    new GeoPoint(48635600, -1510600),
+                    "Mont Saint Michel",
+                    "Mont Saint-Michel is a rocky tidal island and a commune in Normandy, France. It is located approximately one kilometre (just over half a mile) off the country's north-western coast, at the mouth of the Couesnon River near Avranches."),
+            new Annotation(new GeoPoint(48856600, 2351000), "Paris", "The city of love"),
+            new Annotation(new GeoPoint(44837400, -576100), "Bordeaux",
+                    "A port city in southwestern France"),
+            new Annotation(new GeoPoint(48593100, -647500), "Domfront",
+                    "A commune in the Orne department in north-western France"),
+    };
+
+    private static final Annotation[] sEurope = {
+            new Annotation(new GeoPoint(55755800, 37617600), "Moscow"),
+            new Annotation(new GeoPoint(59332800, 18064500), "Stockholm"),
+            new Annotation(new GeoPoint(59939000, 30315800), "Saint Petersburg"),
+            new Annotation(new GeoPoint(60169800, 24938200), "Helsinki"),
+            new Annotation(new GeoPoint(60451400, 22268700), "Turku"),
+            new Annotation(new GeoPoint(65584200, 22154700), "Lule\u00E5"),
+            new Annotation(new GeoPoint(59438900, 24754500), "Talinn"),
+            new Annotation(new GeoPoint(66498700, 25721100), "Rovaniemi"),
+    };
+
+    private static final Annotation[] sUsaWestCoast = {
+            new Annotation(new GeoPoint(40714400, -74006000), "New York City"),
+            new Annotation(new GeoPoint(39952300, -75163800), "Philadelphia"),
+            new Annotation(new GeoPoint(38895100, -77036400), "Washington"),
+            new Annotation(new GeoPoint(41374800, -83651300), "Bowling Green"),
+            new Annotation(new GeoPoint(42331400, -83045800), "Detroit"),
+    };
+
+    private static final Annotation[] sUsaEastCoast = {
+            new Annotation(new GeoPoint(37774900, -122419400), "San Francisco"),
+            new Annotation(new GeoPoint(37770600, -119510800), "Yosemite National Park"),
+            new Annotation(new GeoPoint(36878200, -121947300), "Monteray Bay"),
+            new Annotation(new GeoPoint(35365800, -120849900), "Morro Bay"),
+            new Annotation(new GeoPoint(34420800, -119698200), "Santa Barbara"),
+            new Annotation(new GeoPoint(34052200, -118243700), "Los Angeles"),
+            new Annotation(new GeoPoint(32715300, -117157300), "San Diego"),
+            new Annotation(new GeoPoint(36114600, -115172800), "Las Vegas"),
+            new Annotation(new GeoPoint(36220100, -116881700), "Death Valley"),
+            new Annotation(new GeoPoint(36355200, -112661200), "Grand Canyon"),
+            new Annotation(new GeoPoint(37289900, -113048900), "Zion National Park"),
+            new Annotation(new GeoPoint(37628300, -112167700), "Bryce Canyon"),
+            new Annotation(new GeoPoint(36936900, -111483800), "Lake Powell"),
+    };
+
+    private static final Annotation[][] sRegions = {
+            sFrance, sEurope, sUsaEastCoast, sUsaWestCoast
+    };
+    // @formatter:on
+
+    private PolarisMapView mMapView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!isTaskRoot()) {
-            finish();
-            return;
+        setContentView(R.layout.activity_main);
+
+        mMapView = new PolarisMapView(this, Config.GOOGLE_MAPS_API_KEY);
+        mMapView.setUserTrackingButtonEnabled(true);
+        mMapView.setOnRegionChangedListenerListener(this);
+        mMapView.setOnAnnotationSelectionChangedListener(this);
+
+        // Prepare an alternate pin Drawable
+        final Drawable altMarker = MapViewUtils.boundMarkerCenterBottom(getResources().getDrawable(
+                R.drawable.map_pin_holed_violet));
+
+        // Prepare the list of Annotation using the alternate Drawable for all
+        // Annotation located in France
+        final ArrayList<Annotation> annotations = new ArrayList<Annotation>();
+        for (Annotation[] region : sRegions) {
+            for (Annotation annotation : region) {
+                if (region == sFrance) {
+                    annotation.setMarker(altMarker);
+                }
+                annotations.add(annotation);
+            }
         }
+        mMapView.setAnnotations(annotations, R.drawable.map_pin_holed_blue);
 
-        setContentView(R.layout.activity_delegate);
-
-        doInitStuff();
-
-        // FIXME: Decide which activity to start and then finish. The activities
+        final FrameLayout mapViewContainer = (FrameLayout) findViewById(R.id.map_view_container);
+        mapViewContainer.addView(mMapView, new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT));
     }
 
     @Override
-    protected void onDestroy() {
-        cleanInitStuff();
-        super.onDestroy();
+    protected void onStart() {
+        super.onStart();
+        mMapView.onStart();
     }
 
-    private void doInitStuff() {
-        if (AppParams.hasGcmSupport)
-            registerGcm();
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMapView.onStop();
     }
 
-    private void cleanInitStuff() {
-        if (AppParams.hasGcmSupport)
-            cleanGcm();
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 
-    private void registerGcm() {
-        final String regId = GCMRegistrar.getRegistrationId(this);
-        if (regId.equals("")) {
-            GCMRegistrar.register(this, CustomConstants.GCM_SENDER_ID);
+    @Override
+    public void onRegionChanged(PolarisMapView mapView) {
+        if (Config.INFO_LOGS_ENABLED) {
+            Log.i(LOG_TAG, "onRegionChanged");
+        }
+    }
+
+    @Override
+    public void onRegionChangeConfirmed(PolarisMapView mapView) {
+        if (Config.INFO_LOGS_ENABLED) {
+            Log.i(LOG_TAG, "onRegionChangeConfirmed");
+        }
+    }
+
+    @Override
+    public void onAnnotationSelected(PolarisMapView mapView, MapCalloutView calloutView,
+            int position, Annotation annotation) {
+        if (Config.INFO_LOGS_ENABLED) {
+            Log.i(LOG_TAG, "onAnnotationSelected");
+        }
+        calloutView.setDisclosureEnabled(true);
+        calloutView.setClickable(true);
+        if (!TextUtils.isEmpty(annotation.getSnippet())) {
+            calloutView.setLeftAccessoryView(getLayoutInflater().inflate(R.layout.accessory,
+                    calloutView, false));
         } else {
-            // Device is already registered on GCM, check server.
-            if (GCMRegistrar.isRegisteredOnServer(this)) {
-                // Skips registration.
-                LogUtils.d("Already registered on GCM");
-            } else {
-                // Try to register again, but not in the UI thread.
-                // It's also necessary to cancel the thread onDestroy(),
-                // hence the use of AsyncTask instead of a raw thread.
-                final Context context = this;
-                mRegisterTask = new AsyncTask<Void, Void, Void>() {
-
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        boolean registered = NetService.getInstance()
-                                .registerGcm(regId);
-                        // At this point all attempts to register with the app
-                        // server failed, so we need to unregister the device
-                        // from GCM - the app will try to register again when
-                        // it is restarted. Note that GCM will send an
-                        // unregistered callback upon completion, but
-                        // GCMIntentService.onUnregistered() will ignore it.
-                        if (!registered) {
-                            GCMRegistrar.unregister(context);
-                        }
-                        return null;
-                    }
-
-                    @Override
-                    protected void onPostExecute(Void result) {
-                        mRegisterTask = null;
-                    }
-
-                };
-                mRegisterTask.execute(null, null, null);
-            }
+            calloutView.setLeftAccessoryView(null);
         }
     }
 
-    private void cleanGcm() {
-        if (mRegisterTask != null) {
-            mRegisterTask.cancel(true);
-        }
-        try {
-            GCMRegistrar.onDestroy(this);
-        } catch (IllegalArgumentException iae) {
+    @Override
+    public void onAnnotationDeselected(PolarisMapView mapView, MapCalloutView calloutView,
+            int position, Annotation annotation) {
+        if (Config.INFO_LOGS_ENABLED) {
+            Log.i(LOG_TAG, "onAnnotationDeselected");
         }
     }
+
+    @Override
+    public void onAnnotationClicked(PolarisMapView mapView, MapCalloutView calloutView,
+            int position, Annotation annotation) {
+        if (Config.INFO_LOGS_ENABLED) {
+            Log.i(LOG_TAG, "onAnnotationClicked");
+        }
+        Toast.makeText(this, getString(R.string.annotation_clicked, annotation.getTitle()),
+                Toast.LENGTH_SHORT).show();
+    }
+
 }
