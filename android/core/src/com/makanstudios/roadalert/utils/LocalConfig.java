a3bb70614527504017bab0ddc3a85495ac262224
Catalin Morosan <catalin.morosan@gmail.com>
2012-10-27 21:41:48 +0300

    Forgot to add some files.

---
+++ android/core/src/com/makanstudios/roadalert/utils/LocalConfig.java
@@ -0,0 +1,12 @@
+
+package com.makanstudios.roadalert.utils;
+
+public final class LocalConfig {
+
+    private LocalConfig() {
+    }
+
+    public static final String GOOGLE_MAPS_API_KEY_RELEASE = "00ENaMd-f7ueoinzKXRjXniqxmsmTwi8HFgKzCw";
+    public static final String GOOGLE_MAPS_API_KEY_DEBUG = "00ENaMd-f7udjuiwbDpqJU12P_M4TCh3x2M5aWw";
+
+}
